e16958ff6fcfaa9dfec6855d5ccb2996506ce08d
R Primus <rprimus@gmail.com>
2021-08-03 07:45:09 +0100

    Remove extraneous '='

--- .github/actions/joke-action/main.js
+++ .github/actions/joke-action/main.js
@@ -1,7 +1,7 @@
 const getJoke = require("./joke");
 const core = require("@actions/core");
 
-async function run() = {
+async function run() {
   const joke = await getJoke();
   console.log(joke);
   core.setOutput("joke-output", joke);
