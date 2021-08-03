8fa5f5d9be7589d4a92621649434c159fcb26406
R Primus <rprimus@gmail.com>
2021-08-03 09:38:19 +0100

    Replace import with require

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -1,5 +1,5 @@
-const core = import("@actions/core");
-const github = import("@actions/github");
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 async function run() {
   try {
@@ -7,7 +7,7 @@ async function run() {
     const jokeBody = core.getInput("joke");
     const token = core.getInput("repo-token");
 
-    const octokit = github.getOctokit(token);
+    const octokit = github.getOktokit(token);
 
     const newIssue =await octokit.issues.create({
       repo: github.context.repo.repo,
